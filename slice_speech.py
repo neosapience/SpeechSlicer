03a584819dd0f61745e8c6c591fc1259d997a0b3
srafsasm <71336788+srafsasm@users.noreply.github.com>
2024-08-12 18:30:02 +0900

    remove redundant arguments

--- slice_speech.py
+++ slice_speech.py
@@ -9,8 +9,6 @@ def parse_arguments():
     parser.add_argument("--overwrite", default=False, action="store_true")
     parser.add_argument("--model", type=str, default="gpt-4o-mini", help="OpenAI model for LLM merging")
     parser.add_argument("--llm_merging", default=False, action="store_true")
-    parser.add_argument("--subtitle_available", default=False, action="store_true", help="If you have manual subtitles from Youtube, we can obtain a better transcription from the subtitles. Set `subtitle_path` option where the files are in the same format with `input_path_style`.")
-    parser.add_argument("--subtitle_path", default=None, help="Parent path where the subtitles are saved in. The format must be the same with `input_path_style`, file name must be same with video/audio, only replacing the extension with \"vtt\".")
     parser.add_argument("--output_path", type=str, default="./output")
     parser.add_argument("--padding", type=float, default=0.5, help="Maximum length of the padding to be added")
     parser.add_argument("--recursive_depth", type=int, default=2, help="How many times whisper will run. 2 is recommended.")
